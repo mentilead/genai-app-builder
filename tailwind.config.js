d36d2b30e1fba9a9c56d85dac5b328563b4db2ef
EC2 Default User <ec2-user@ip-10-0-31-189.eu-central-1.compute.internal>
2024-03-26 09:14:38 +0000

    More gui fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,6 +50,17 @@ module.exports = {
                 primary: {"50":"#f0fdfa","100":"#ccfbf1","200":"#99f6e4","300":"#5eead4","400":"#2dd4bf","500":"#14b8a6","600":"#0d9488","700":"#0f766e","800":"#115e59","900":"#134e4a","950":"#042f2e"},
                 'light-bg-primary': '#f5f5f5',
                 'dark-bg-primary': '#121826',
+                'light-bg-secondary': '#ffffff',
+                'dark-bg-secondary': '#181f39',
+
+                'light-text-primary': '#394150',
+                'dark-text-primary': '#d2d5da',
+                'light-text-secondary': '#394150',
+                'dark-text-secondary': '#97A3B6',
+
+                'light-border-primary': '#cdcfd1',
+                'dark-border-primary': '#4a5568',
+
             },
             fontFamily: {
                 'body': [
