303ee1f65b4e1c4987fdefe08b1f3953bc15c47b
EC2 Default User <ec2-user@ip-10-0-31-189.eu-central-1.compute.internal>
2024-03-27 08:22:30 +0000

    Gui stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,7 +47,19 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary: {"50":"#f0fdfa","100":"#ccfbf1","200":"#99f6e4","300":"#5eead4","400":"#2dd4bf","500":"#14b8a6","600":"#0d9488","700":"#0f766e","800":"#115e59","900":"#134e4a","950":"#042f2e"},
+                primary: {
+                    "50": "#f0fdfa",
+                    "100": "#ccfbf1",
+                    "200": "#99f6e4",
+                    "300": "#5eead4",
+                    "400": "#2dd4bf",
+                    "500": "#14b8a6",
+                    "600": "#0d9488",
+                    "700": "#0f766e",
+                    "800": "#115e59",
+                    "900": "#134e4a",
+                    "950": "#042f2e"
+                },
                 'light-bg-primary': '#f5f5f5',
                 'dark-bg-primary': '#121826',
                 'light-bg-secondary': '#ffffff',
@@ -98,6 +110,47 @@ module.exports = {
                     'Noto Color Emoji'
                 ]
             },
+            typography: ({theme}) => ({
+                orange: {
+                    css: {
+                        '--tw-format-body': theme('colors.orange[500]'),
+                        '--tw-format-headings': theme('colors.orange[900]'),
+                        '--tw-format-lead': theme('colors.orange[500]'),
+                        '--tw-format-links': theme('colors.orange[600]'),
+                        '--tw-format-bold': theme('colors.orange[900]'),
+                        '--tw-format-counters': theme('colors.orange[500]'),
+                        '--tw-format-bullets': theme('colors.orange[500]'),
+                        '--tw-format-hr': theme('colors.orange[200]'),
+                        '--tw-format-quotes': theme('colors.orange[900]'),
+                        '--tw-format-quote-borders': theme('colors.orange[300]'),
+                        '--tw-format-captions': theme('colors.orange[700]'),
+                        '--tw-format-code': theme('colors.orange[900]'),
+                        '--tw-format-code-bg': theme('colors.orange[50]'),
+                        '--tw-format-pre-code': theme('colors.orange[100]'),
+                        '--tw-format-pre-bg': theme('colors.orange[900]'),
+                        '--tw-format-th-borders': theme('colors.orange[300]'),
+                        '--tw-format-td-borders': theme('colors.orange[200]'),
+                        '--tw-format-th-bg': theme('colors.orange[50]'),
+                        '--tw-format-invert-body': theme('colors.orange[200]'),
+                        '--tw-format-invert-headings': theme('colors.white'),
+                        '--tw-format-invert-lead': theme('colors.orange[300]'),
+                        '--tw-format-invert-links': theme('colors.white'),
+                        '--tw-format-invert-bold': theme('colors.white'),
+                        '--tw-format-invert-counters': theme('colors.orange[400]'),
+                        '--tw-format-invert-bullets': theme('colors.orange[600]'),
+                        '--tw-format-invert-hr': theme('colors.orange[700]'),
+                        '--tw-format-invert-quotes': theme('colors.pink[100]'),
+                        '--tw-format-invert-quote-borders': theme('colors.orange[700]'),
+                        '--tw-format-invert-captions': theme('colors.orange[400]'),
+                        '--tw-format-invert-code': theme('colors.white'),
+                        '--tw-format-invert-pre-code': theme('colors.orange[300]'),
+                        '--tw-format-invert-pre-bg': 'rgb(0 0 0 / 50%)',
+                        '--tw-format-invert-th-borders': theme('colors.orange[600]'),
+                        '--tw-format-invert-td-borders': theme('colors.orange[700]'),
+                        '--tw-format-invert-th-bg': theme('colors.orange[700]'),
+                    },
+                },
+            }),
         },
     },
     plugins: [
